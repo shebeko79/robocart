1c78f373c50ff6a941ad7d41e4f331009d457a1a
shebeko79 <eugene.shebeko@gmail.com>
2026-06-27 16:50:15 +0200

    Figured out how to draw on top of the stream

--- proba/rtsp.py
+++ proba/rtsp.py
@@ -14,6 +14,9 @@ print(server.get_url())
 cam_yolo = cam.add_channel(320, 320)
 yolo_model = nn.YOLO11(model="/root/models/yolo11n_320_room.mud")
 
+rgn = server.add_region(0, 0, 640, 640)
+
+
 while True:
     img = cam_yolo.read()
 
@@ -30,6 +33,11 @@ while True:
         img_rgb.draw_rect(obj.x, obj.y, obj.w, obj.h, color=image.COLOR_RED)
         msg = f'{yolo_model.labels[obj.class_id]}: {obj.score:.2f}'
         img_rgb.draw_string(obj.x, obj.y, msg, color=image.COLOR_RED)
+
+        cnv = rgn.get_canvas()
+        cnv.draw_rect(obj.x*2, obj.y*2, obj.w*2, obj.h*2, color=image.COLOR_RED, thickness=2)
+        cnv.draw_string(obj.x*2, obj.y*2, msg, color=image.COLOR_RED)
+        rgn.update_canvas()
 
     disp.show(img_rgb)
 
