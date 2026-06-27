5445e6686774f88c231772ff103d062fc0a2681f
shebeko79 <eugene.shebeko@gmail.com>
2026-06-27 15:42:41 +0200

    Surprisingly, I was able to add low resolution channel and it works and already in RGB. I don't even need OpenCV to convert the image

--- proba/rtsp.py
+++ proba/rtsp.py
@@ -3,34 +3,27 @@ import numpy as np
 import cv2
 
 #cam = camera.Camera(1920, 1080, image.Format.FMT_YVU420SP)
-#cam = camera.Camera(640, 640, image.Format.FMT_YVU420SP)
-cam = camera.Camera(320, 320, image.Format.FMT_YVU420SP)
+cam = camera.Camera(640, 640, image.Format.FMT_YVU420SP)
 disp = display.Display()
 
 server = rtsp.Rtsp()
 server.bind_camera(cam)
 server.start()
-
 print(server.get_url())
 
+cam_yolo = cam.add_channel(320, 320)
 yolo_model = nn.YOLO11(model="/root/models/yolo11n_320_room.mud")
 
 while True:
-    img = cam.read()
+    img = cam_yolo.read()
 
     tm = time.time_ms()
 
     w = img.width()
     h = img.height()
 
-    yuv = np.frombuffer(img.to_bytes(), dtype=np.uint8)
-    #print(f'frombuffer()={time.time_ms() - tm}')
-    yuv = yuv.reshape((h * 3 // 2, w))
-    #print(f'reshape()={time.time_ms() - tm}')
-    rgb = cv2.cvtColor(yuv, cv2.COLOR_YUV2RGB_NV21)
-    #print(f'cvtColor()={time.time_ms() - tm}')
-    img_rgb = image.cv2image(rgb, bgr=False, copy=False)
-    #print(f'cv2image()={time.time_ms() - tm}')
+    print(f'{w=} {h=}')
+    img_rgb = img
 
     objs = yolo_model.detect(img_rgb, conf_th=0.5, iou_th=0.45)
     for obj in objs:
